a56e69a11d295e6e55bebb5f077d8a8d35124130
Joel Dynamo <dynamo.joey@gmail.com>
2019-05-03 16:00:24 +0000

    Add new Message and createMessage Events

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+var socket = io();
+
+socket.on('connect',function ()  {
+    console.log('Connected to Server');
+
+    socket.emit('createMessage', {
+        from: 'Jane',
+        text: 'Its working for my good'
+    });
+});
+
+
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server')
+})
+
+// socket.on('newEmail',function (email){
+//     console.log('New email',email);
+// })
+
+socket.on('newMessage',function (message){
+    console.log('newMessage',message)
+})
--- server/server.js
+++ server/server.js
@@ -15,6 +15,27 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('New user connected');
 
+    socket.emit('newMessage',{
+        from:'john',
+        text:'See you soon God willing',
+        createdAt:12312
+    })
+
+    //Emitters and listeners(server side)
+    // socket.emit('newEmail',{
+    //     from:'mike@example.com',
+    //     text:'Hey whats going on',
+    //     createdAt:123
+    // })
+
+    socket.on('createEmail',(newEmail)=>{
+        console.log('createEmail',newEmail);
+    })
+
+    socket.on('createMessage',(message)=>{
+        console.log('createMessage',message);
+    })
+
     socket.on('disconnect', () => {
         console.log('Disconnected from server')
     })
