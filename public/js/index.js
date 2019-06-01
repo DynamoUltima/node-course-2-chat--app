fca7199ec977b7b61b682fa0398d1fab954478c2
Joel Dynamo <dynamo.joey@gmail.com>
2019-06-01 11:38:49 +0000

    Scroll To Bottom added

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
-//var moment = require('moment');
+
+
+function scrollToBottom(){
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child')
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+
+    var newMessageHeight =newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight()
+
+    if(clientHeight +scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+
+}
 
 socket.on('connect', function () {
     console.log('Connected to Server');
@@ -27,6 +44,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
     
  })
 
@@ -40,6 +58,8 @@ socket.on('newLocationMessage', function (message) {
         createdAt: formattedTime
     })
     jQuery('#messages').append(html);
+    scrollToBottom(); 
+
 
     // var li = jQuery('<li></li>')
     // var a = jQuery('<a target ="_blank">My Current Location</a>')
